af7e0c60347f4f8e148f56acb7822290b442bb7b
HermesSantos <hermessantos995@gmail.com>
2025-03-27 13:27:14 -0300

    second one

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 func main () {
   cmd := exec.Command("git", "diff")
   output, err := cmd.Output()
+
   if err != nil {
     fmt.Println("Error: ", err)
     return
   }
+
   fmt.Println(string(output))
 }
