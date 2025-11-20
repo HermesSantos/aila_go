9780f3f1194297069a6bbb1f5223bcf53a2d10df
HermesSantos <hermessantos995@gmail.com>
2025-11-20 20:30:40 -0300

    Fix database path for different execution modes

--- src/config/db.go
+++ src/config/db.go
@@ -3,6 +3,9 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -10,9 +13,25 @@ import (
 var DB *sql.DB
 
 func InitDatabase() {
-	var err error
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	DB, err = sql.Open("sqlite3", "./database.db")
+	execDir := filepath.Dir(execPath)
+
+	isGoRun := strings.Contains(execDir, "go-build")
+
+	var dbPath string
+
+	if isGoRun {
+		wd, _ := os.Getwd()
+		dbPath = filepath.Join(wd, "database.db")
+	} else {
+		dbPath = filepath.Join(execDir, "database.db")
+	}
+
+	DB, err = sql.Open("sqlite3", dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,8 +40,7 @@ func InitDatabase() {
 	CREATE TABLE IF NOT EXISTS user_data (
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		api_key TEXT
-	);
-	`
+	);`
 
 	_, execErr := DB.Exec(sqlStmt)
 	if execErr != nil {
